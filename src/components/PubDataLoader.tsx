76dd735181fd3fe6c60f55c1d590e9863eeb0589
Pub Club Developer <fredd@pubclub.co.uk>
2025-10-14 20:09:34 +0100

    Clean up map UI and restore colorful map - Remove 'X pubs visible on map' text - Remove monochrome map styling - Restore colorful Google Maps default appearance - Cleaner, less cluttered map interface

--- src/components/PubDataLoader.tsx
+++ src/components/PubDataLoader.tsx
@@ -157,78 +157,9 @@ export default function PubDataLoader() {
       const timer = setTimeout(() => {
         if (mapDivRef.current && !map) {
           try {
-            // Monochrome map style
-            const monoStyle = [
-              { elementType: "geometry", stylers: [{ color: "#f5f5f5" }] },
-              { elementType: "labels.icon", stylers: [{ visibility: "off" }] },
-              { elementType: "labels.text.fill", stylers: [{ color: "#616161" }] },
-              { elementType: "labels.text.stroke", stylers: [{ color: "#f5f5f5" }] },
-              {
-                featureType: "administrative.land_parcel",
-                elementType: "labels.text.fill",
-                stylers: [{ color: "#bdbdbd" }],
-              },
-              {
-                featureType: "poi",
-                elementType: "geometry",
-                stylers: [{ color: "#eeeeee" }],
-              },
-              {
-                featureType: "poi",
-                elementType: "labels.text.fill",
-                stylers: [{ color: "#757575" }],
-              },
-              {
-                featureType: "poi",
-                elementType: "labels",
-                stylers: [{ visibility: "off" }],
-              },
-              {
-                featureType: "road",
-                elementType: "geometry",
-                stylers: [{ color: "#ffffff" }],
-              },
-              {
-                featureType: "road.arterial",
-                elementType: "labels.text.fill",
-                stylers: [{ color: "#757575" }],
-              },
-              {
-                featureType: "road.highway",
-                elementType: "geometry",
-                stylers: [{ color: "#dadada" }],
-              },
-              {
-                featureType: "road.highway",
-                elementType: "labels.text.fill",
-                stylers: [{ color: "#616161" }],
-              },
-              {
-                featureType: "transit",
-                elementType: "geometry",
-                stylers: [{ color: "#e5e5e5" }],
-              },
-              {
-                featureType: "transit",
-                elementType: "labels",
-                stylers: [{ visibility: "off" }],
-              },
-              {
-                featureType: "water",
-                elementType: "geometry",
-                stylers: [{ color: "#c9c9c9" }],
-              },
-              {
-                featureType: "water",
-                elementType: "labels.text.fill",
-                stylers: [{ color: "#9e9e9e" }],
-              },
-            ];
-
             const newMap = new window.google.maps.Map(mapDivRef.current, {
               center: { lat: 51.5074, lng: -0.1278 }, // London center
               zoom: 11,
-              styles: monoStyle,
               streetViewControl: false,
               mapTypeControl: false,
               fullscreenControl: true,
@@ -371,16 +302,7 @@ export default function PubDataLoader() {
                   </span>
                   )}
                 </>
-              ) : (
-                <>
-                  <span className="font-semibold">{markerCount}</span> pubs visible on map
-                  {mapTotalPubs > markerCount && (
-                    <span className="text-gray-500 ml-1">
-                      (total: {mapTotalPubs} pubs)
-                    </span>
-                  )}
-                </>
-                )}
+              ) : null}
               </div>
 
             {/* View Mode Toggle */}
